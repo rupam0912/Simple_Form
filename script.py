94181e78ff806e5da56235aee05969199d617cf9
rupam0912 <40741512+rupam0912@users.noreply.github.com>
2019-11-13 01:10:28 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import time
+browser = webdriver.Chrome('/home/rupam/Downloads/rpa_demo/chromedriver')
+browser.get('https://rupam0912.github.io') 
+
+
+#browser.find_element_by_xpath("//input[@id ='submit_id']").click()
+
+#a = browser.find_element_by_xpath("//input[@id ='naam_werkgever_ld']") 
+#a.send_keys("")
+
+import pandas as pd
+df = pd.read_csv("test.csv")
+a = browser.find_element_by_xpath("//input[@id ='naam_werkgever_ld']")
+b = browser.find_element_by_xpath("//input[@id ='plaats_werkgever_ld']")
+c = browser.find_element_by_xpath("//input[@id ='functie_ld']")
+d = browser.find_element_by_xpath("//input[@id ='startdate_job']")
+e = browser.find_element_by_xpath("//input[@id ='enddate_ld_job']")
+
+for i in range(len(df)) :
+    a.send_keys(df.iloc[i,0])
+    b.send_keys(df.iloc[i,1])
+    c.send_keys(df.iloc[i,2])
+    d.send_keys(df.iloc[i,3])
+    e.send_keys(str(df.iloc[i,4]))
+    
+    browser.find_element_by_xpath("//input[@id ='submit_id']").click()
+    time.sleep(5)
+    
+
+
+
+
